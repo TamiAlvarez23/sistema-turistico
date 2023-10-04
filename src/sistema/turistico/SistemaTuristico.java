ece08a90c7cf5a96e7c4332e99c02fefdc6fe418
gustavo salgado <agustinbritoscba1984@gmail.com>
2023-10-03 23:51:28 -0300

    Update SistemaTuristico.java

--- src/sistema/turistico/SistemaTuristico.java
+++ src/sistema/turistico/SistemaTuristico.java
@@ -17,9 +17,7 @@ public class SistemaTuristico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Crear una instancia de CiudadData, AlojamientoData, PasajeData, y PaqueteData
-        // Asegúrate de tener una conexión a la base de datos configurada correctamente
-
+       
         CiudadData ciudadData = new CiudadData();
         AlojamientoData alojamientoData = new AlojamientoData();
         PasajeData pasajeData = new PasajeData(/* conexión a la base de datos */);
@@ -42,20 +40,16 @@ public class SistemaTuristico {
 
             switch (opcion) {
                 case "1":
-                    // Agregar una Ciudad
-                    // Solicitar datos al usuario y usar ciudadData para agregar la Ciudad a la base de datos
+                    
                     break;
                 case "2":
-                    // Agregar un Alojamiento
-                    // Solicitar datos al usuario y usar alojamientoData para agregar el Alojamiento a la base de datos
+                    
                     break;
                 case "3":
-                    // Agregar un Pasaje
-                    // Solicitar datos al usuario y usar pasajeData para agregar el Pasaje a la base de datos
+                    
                     break;
                 case "4":
-                    // Agregar un Paquete
-                    // Solicitar datos al usuario y usar paqueteData para agregar el Paquete a la base de datos
+                    
                     break;
                 case "5":
                     // Salir del programa
