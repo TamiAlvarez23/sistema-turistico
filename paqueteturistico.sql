b83dbb381feaf5c6292b64728497802436c19333
Tamara <141647901+TamiAlvarez23@users.noreply.github.com>
2023-10-18 11:34:43 -0300

    Add files via upload

---
+++ paqueteturistico.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-10-2023 a las 16:33:06
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `paqueteturistico`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alojamiento`
+--
+
+CREATE TABLE `alojamiento` (
+  `idAlojamiento` int(11) NOT NULL,
+  `fechaIngreso` date NOT NULL,
+  `fechaEgreso` date NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `servicio` varchar(100) NOT NULL,
+  `importeDiario` double NOT NULL,
+  `cupoAlojamiento` int(11) NOT NULL,
+  `ciudadDestino` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudad`
+--
+
+CREATE TABLE `ciudad` (
+  `idCiudad` int(15) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `pais` varchar(100) NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `provincia` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paquete`
+--
+
+CREATE TABLE `paquete` (
+  `idPaquete` int(15) NOT NULL,
+  `origen` int(15) NOT NULL,
+  `destino` int(15) NOT NULL,
+  `alojamiento` int(15) NOT NULL,
+  `pasaje` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pasaje`
+--
+
+CREATE TABLE `pasaje` (
+  `idPasaje` int(11) NOT NULL,
+  `tipoTransporte` varchar(100) NOT NULL,
+  `importe` double NOT NULL,
+  `nombreCiudadOrigen` int(11) NOT NULL,
+  `cupoPasaje` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(100) NOT NULL,
+  `nombreUsuario` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `claveUsuario` varchar(100) NOT NULL,
+  `estadoUsuario` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alojamiento`
+--
+ALTER TABLE `alojamiento`
+  ADD PRIMARY KEY (`idAlojamiento`),
+  ADD KEY `ciudadDestino` (`ciudadDestino`);
+
+--
+-- Indices de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD PRIMARY KEY (`idCiudad`);
+
+--
+-- Indices de la tabla `paquete`
+--
+ALTER TABLE `paquete`
+  ADD PRIMARY KEY (`idPaquete`),
+  ADD KEY `paquete_alojamiento_foreign` (`alojamiento`),
+  ADD KEY `paquete_pasaje_foreign` (`pasaje`),
+  ADD KEY `origen` (`origen`),
+  ADD KEY `destino` (`destino`);
+
+--
+-- Indices de la tabla `pasaje`
+--
+ALTER TABLE `pasaje`
+  ADD PRIMARY KEY (`idPasaje`),
+  ADD KEY `pasaje_nombreCiudadOrigen_foreign` (`nombreCiudadOrigen`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alojamiento`
+--
+ALTER TABLE `alojamiento`
+  MODIFY `idAlojamiento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  MODIFY `idCiudad` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `paquete`
+--
+ALTER TABLE `paquete`
+  MODIFY `idPaquete` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pasaje`
+--
+ALTER TABLE `pasaje`
+  MODIFY `idPasaje` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alojamiento`
+--
+ALTER TABLE `alojamiento`
+  ADD CONSTRAINT `alojamiento_ibfk_1` FOREIGN KEY (`ciudadDestino`) REFERENCES `ciudad` (`idCiudad`);
+
+--
+-- Filtros para la tabla `paquete`
+--
+ALTER TABLE `paquete`
+  ADD CONSTRAINT `paquete_ibfk_1` FOREIGN KEY (`origen`) REFERENCES `ciudad` (`idCiudad`),
+  ADD CONSTRAINT `paquete_ibfk_2` FOREIGN KEY (`destino`) REFERENCES `ciudad` (`idCiudad`),
+  ADD CONSTRAINT `paquete_ibfk_3` FOREIGN KEY (`alojamiento`) REFERENCES `alojamiento` (`idAlojamiento`) ON DELETE CASCADE,
+  ADD CONSTRAINT `paquete_pasaje_foreign` FOREIGN KEY (`pasaje`) REFERENCES `pasaje` (`idPasaje`);
+
+--
+-- Filtros para la tabla `pasaje`
+--
+ALTER TABLE `pasaje`
+  ADD CONSTRAINT `pasaje_nombreCiudadOrigen_foreign` FOREIGN KEY (`nombreCiudadOrigen`) REFERENCES `ciudad` (`idCiudad`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
