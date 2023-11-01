d3dc50b17310cc43a7a91da2af0d02bf6ed26125
Tamara <tamih.alvarez@gmail.com>
2023-11-01 03:02:36 -0300

    nueva BD

--- paqueteturistico.sql
+++ paqueteturistico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-10-2023 a las 02:22:36
+-- Tiempo de generación: 01-11-2023 a las 06:58:22
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -40,6 +40,18 @@ CREATE TABLE `alojamiento` (
   `tipoAlojamiento` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `alojamiento`
+--
+
+INSERT INTO `alojamiento` (`idAlojamiento`, `fechaIngreso`, `fechaEgreso`, `estado`, `servicio`, `importeDiario`, `cupoAlojamiento`, `ciudadDestino`, `nombre`, `tipoAlojamiento`) VALUES
+(1, '2023-02-21', '2023-03-23', 1, 'Desayuno', 20000, 45, 5, 'Santa Teresita', 'Hotel'),
+(2, '2023-02-21', '2023-03-23', 1, 'Play', 4000, 78, 5, 'San Nicolas', 'Hotel'),
+(3, '2023-01-01', '2023-01-01', 1, 'Aire Acondicionado', 40000, 45, 14, 'San Espedito', 'Hotel'),
+(4, '2023-02-21', '2023-03-23', 1, 'Almuerzo', 7500, 50, 18, 'San Agustin', 'Hotel'),
+(5, '2023-02-21', '2023-03-23', 1, 'Desayuno', 75000, 75, 14, 'Santa Tamara', 'Hostel'),
+(6, '2023-01-01', '2023-01-01', 1, 'wifi', 15000, 45, 5, 'Santa Clara', 'Hostel');
+
 -- --------------------------------------------------------
 
 --
@@ -59,11 +71,9 @@ CREATE TABLE `ciudad` (
 --
 
 INSERT INTO `ciudad` (`idCiudad`, `nombre`, `pais`, `estado`, `provincia`) VALUES
-(5, 'Agustin Colongne', 'Argentina', 1, 'Cordoba'),
-(6, 'Villa Gecel', 'Argentina', 1, 'Buenos Aires'),
-(7, 'Mina Clavero', 'Argentina', 1, 'Cordoba'),
-(8, 'Cura Brochero', 'Argentina', 1, 'Cordoba'),
-(9, 'Alta gracia', 'Argentina', 1, 'Corodba');
+(5, 'Carlos Paz', 'Argentina', 1, 'Cordoba'),
+(14, 'Alta Gracia', 'Argentina', 1, 'Cordoba'),
+(18, 'Mina Clavero', 'Argentina', 1, 'Cordoba');
 
 -- --------------------------------------------------------
 
@@ -76,8 +86,21 @@ CREATE TABLE `paquete` (
   `origen` int(15) NOT NULL,
   `destino` int(15) NOT NULL,
   `alojamiento` int(15) NOT NULL,
-  `pasaje` int(15) NOT NULL
+  `pasaje` int(15) NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `cupo` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `paquete`
+--
+
+INSERT INTO `paquete` (`idPaquete`, `origen`, `destino`, `alojamiento`, `pasaje`, `estado`, `cupo`) VALUES
+(1, 5, 5, 1, 2, 1, 0),
+(2, 5, 5, 1, 2, 1, 0),
+(3, 5, 5, 3, 5, 1, 0),
+(4, 5, 5, 1, 2, 1, 0),
+(5, 14, 5, 1, 6, 1, -30);
 
 -- --------------------------------------------------------
 
@@ -91,10 +114,27 @@ CREATE TABLE `pasaje` (
   `importe` double NOT NULL,
   `nombreCiudadOrigen` int(11) NOT NULL,
   `cupoPasaje` int(11) NOT NULL,
-  `estado` tinyint(1) NOT NULL,
-  `fechaSalida` date NOT NULL,
-  `fechaRegreso` date NOT NULL
+  `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pasaje`
+--
+
+INSERT INTO `pasaje` (`idPasaje`, `tipoTransporte`, `importe`, `nombreCiudadOrigen`, `cupoPasaje`, `estado`) VALUES
+(2, 'Avión', 8, 5, 0, 1),
+(3, 'Avión', 4, 5, 70, 1),
+(4, 'Tren', 7400, 18, 54, 1),
+(5, 'Avión', 7, 5, 0, 1),
+(6, 'Colectivo', 5600, 14, 30, 1),
+(7, 'Avión', 2, 14, 20, 1),
+(8, 'Colectivo', 8, 5, 40, 1),
+(9, 'Colectivo', 8, 14, 8, 1),
+(10, 'Colectivo', 22, 5, 70, 1),
+(11, 'Avión', 888, 5, 8, 1),
+(12, 'Tren', 2, 5, 2, 1),
+(13, 'Avión', 4500, 5, 45, 1),
+(14, 'Colectivo', 7800, 5, 45, 1);
 
 -- --------------------------------------------------------
 
@@ -196,25 +236,25 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `alojamiento`
 --
 ALTER TABLE `alojamiento`
-  MODIFY `idAlojamiento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idAlojamiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `ciudad`
 --
 ALTER TABLE `ciudad`
-  MODIFY `idCiudad` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idCiudad` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `paquete`
 --
 ALTER TABLE `paquete`
-  MODIFY `idPaquete` int(15) NOT NULL AUTO_INCREMENT;
+  MODIFY `idPaquete` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `pasaje`
 --
 ALTER TABLE `pasaje`
-  MODIFY `idPasaje` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idPasaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `presupuesto`
